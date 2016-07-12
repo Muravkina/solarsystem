46c0466f0c5d5fceb4a1ff4dfb40d56bd5bb6317
Darya Murauyova <muravkina@yahoo.com>
2016-07-12 17:58:48 -0400

    horizontal scrolling

--- app/App.js
+++ app/App.js
@@ -3,15 +3,39 @@ import {render} from 'react-dom';
 import solarSystem from '../data/solar_system.js';
 
 class Solar extends Component {
+  constructor(){
+    super();
+    this.state = {
+        width: 7,
+        marginLeft:8
+    }
+  }
   render() {
-  	// var bla = solarSystem.map((item) => {
-  	// 	console.log(item)
-   //  		return (<li>{item.diameter}</li>)
-   //  	})
+  	var planets = solarSystem.map((planet) => {
+  		var image = "images/" + planet.image
+    		return (<div className='planet'>
+                  <h1>{planet.name}</h1>
+                  <img src= {image}/>
+                  <div className="description">
+                    <ul>
+                      <li>{planet.diameter}</li>
+                      <li>{planet.lengthOfDay}</li>
+                      <li>{planet.distanceFromEarth} {planet.shuttleTraveled}</li>
+                    </ul>
+                  </div>
+                </div>
+              )
+    	})
     return(
-    	<div></div>
+    	<div className='solar_system_wrap'>{planets}</div>
     );
   }
 }
+class Description extends Component {
 
+}
+
+class Image extends Components {
+
+}
 render(<Solar />, document.getElementById('root'));
--- data/solar_system.js
+++ data/solar_system.js
@@ -1,9 +1,12 @@
 var sun = {
+	name: 'Sun',
+	category: 'star',
 	diameter: 864938,
 	diameterRatio: 109,
 	distanceFromEarth: 156,
 	distanceFromSun: 0,
-	gravitationalFactor: 27.9
+	gravitationalFactor: 27.9,
+	image: 'sun.png'
 }
 
 // diameter, distance from Sun in miles
@@ -11,103 +14,133 @@ var sun = {
 // distance from Earth in days
 
 var mercury = {
+	name: 'Mercury',
+	category: 'planet',
 	diameter: 3032,
 	diameterRatio: 0.383,
 	lengthOfDay: 4222.6,
 	distanceFromSun: 36000000,
 	distanceFromEarth: 147,
-	shuttleTraveled: 'Mariner', 
+	shuttleTraveled: 'Mariner',
 	gravitationalFactor: 0.378,
-	notes: 'If you want to actually travel there, you need to slow down to its orbital speed, which takes about 6.5 years total (Messenger).'
+	notes: 'If you want to actually travel there, you need to slow down to its orbital speed, which takes about 6.5 years total (Messenger).',
+	image: 'mercury.svg'
 }
 
 var venus = {
+	name: 'Venus',
+	category: 'planet',
 	diameter: 7521,
 	diameterRatio: 0.949,
-	lengthOfDay: 2802, 
+	lengthOfDay: 2802,
 	distanceFromSun: 67200000,
 	distanceFromEarth: 456,
 	shuttleTraveled: 'Magellan',
-	gravitationalFactor: 0.907
+	gravitationalFactor: 0.907,
+	image: 'venus.svg'
 }
 
 var moon = {
+	name: 'Moon',
+	category: 'moon',
 	diameter: 2159,
 	diameterRatio: 0.2724,
 	lengthOfDay: 708.7,
 	distanceFromEarth: 3,
 	shuttleTraveled: 'Apollo 11',
-	gravitationalFactor: 0.166
+	gravitationalFactor: 0.166,
+	image: 'moon.svg'
 }
 
 var earth = {
+	name: 'Earth',
+	category: 'planet',
 	diameter: 7926,
 	diameterRatio: 1,
 	lengthOfDay: 24,
 	distanceFromEarth: 0,
 	shuttleTraveled: '',
 	distanceFromSun: 93000000,
-	gravitationalFactor: 1
+	gravitationalFactor: 1,
+	image: 'earth.svg'
 }
 
 var mars = {
+	name: 'Mars',
+	category: 'planet',
 	diameter: 4221,
 	diameterRatio: 0.532,
 	lengthOfDay: 24.7,
 	distanceFromSun: 141600000,
 	distanceFromEarth: 213,
 	shuttleTraveled: 'Opportunity',
-	gravitationalFactor: 0.377
+	gravitationalFactor: 0.377,
+	image: 'mars.svg'
 }
 
 var jupiter = {
+	name: 'Jupiter',
+	category: 'planet',
 	diameter: 88846,
 	diameterRatio: 11.21,
 	lengthOfDay: 9.9,
 	distanceFromSun: 483800000,
 	distanceFromEarth: 1825,
 	shuttleTraveled: 'Juno',
-	gravitationalFactor: 2.36
+	gravitationalFactor: 2.36,
+	image: 'jupiter.svg'
 }
 
 var saturn = {
+	name: 'Saturn',
+	category: 'planet',
 	diameter: 74897,
 	diameterRatio: 9.45,
 	lengthOfDay: 10.7,
 	distanceFromSun: 890800000,
 	distanceFromEarth: 2555,
 	shuttleTraveled: 'Cassini',
-	gravitationalFactor: 0.916
+	gravitationalFactor: 0.916,
+	image: 'saturn.svg'
 }
 
 var uranus = {
+	name: 'Uranus',
+	category: 'planet',
 	diameter: 31763,
 	diameterRatio: 4.01,
 	lengthOfDay: 17.2,
 	distanceFromSun: 1784800000,
 	distanceFromEarth: 3103,
 	shuttleTraveled: 'Voyager',
-	gravitationalFactor: 0.889
+	gravitationalFactor: 0.889,
+	image: 'uranus.svg'
 }
 
 var neptune = {
+	name: 'Neptune',
+	category: 'planet',
 	diameter: 30775,
 	diameterRatio: 3.88,
 	lengthOfDay: 16.1,
 	distanceFromSun: 2793100000,
 	distanceFromEarth: 4380,
 	shuttleTraveled: 'Voyager',
-	gravitationalFactor: 1.12
+	gravitationalFactor: 1.12,
+	image: 'neptune.svg'
 }
 
 var pluto = {
+	name: 'Pluto',
+	category: 'planet',
 	diameter: 1464,
 	diameterRatio: 0.186,
 	lengthOfDay: 153.3,
 	distanceFromSun: 3670000000,
 	distanceFromEarth: 3468,
 	shuttleTraveled: 'New Horizons',
-	gravitationalFactor: 0.071
+	gravitationalFactor: 0.071,
+	image: 'pluto.svg'
 }
 
 var solarSystem = [sun, mercury, venus, moon, earth, mars, jupiter, saturn, uranus, neptune, pluto];
