87b2003b761d2b87f3b2875bcbf2078bb11e4cc0
Darya Murauyova <muravkina@yahoo.com>
2016-07-14 13:24:15 -0400

    react classes setup

--- app/App.js
+++ app/App.js
@@ -6,16 +6,15 @@ class Solar extends Component {
   constructor(){
     super();
     this.state = {
-        width: 0,
         marginLeft:0
     }
   }
   render() {
-  	var planets = solarSystem.map((planet) => {
+  	var planets = solarSystem.map((planet , i) => {
   		// var image = "images/" + planet.name.toLowerCase() + '.png'
     		return (
               // Planet props
-              <Planet planet={planet} />
+              <Planet key={i} planet={planet} distanceToSun={this.state.marginLeft}/>
               )
     	})
     return(
@@ -25,30 +24,35 @@ class Solar extends Component {
 }
 class Planet extends Component {
   render() {
-    console.log(this.props)
+    var divStyle = {
+      left: this.props.distanceToSun
+    }
+    return (
+      <div className='planet' className={this.props.planet.name} style={divStyle} >
+        <h1>{this.props.planet.name}</h1>
+        <Image name={this.props.planet} />
+        <Description planet={this.props.planet} />
+      </div>
+    );
+  }
+}
+
+class Description extends Component {
+  // initial state = info
+  //if state changes (once someone clicked on it - the state should change)
+  render() {
     return (
       <div></div>
     );
   }
 }
 
-// class Description extends Component {
-//   initial state = info
-//   //if state changes (once someone clicked on it - the state should change)
-//   render() {
-//     return (
-//       <p>Info</p>
-//       <div></div>
-//     );
-//   }
-// }
-
-// class Image extends Components{
-//   //if moon - different position
-//   render() {
-//     return (
-//       <img width>
-//     );
-//   }
-// }
+class Image extends Component {
+  //if moon - different position
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
 render(<Solar />, document.getElementById('root'));
