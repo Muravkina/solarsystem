5ad37ceae34257b849c000f6e2e4277fd1fd2c8d
Darya Murauyova <muravkina@yahoo.com>
2016-11-11 15:57:19 -0500

    transition group

--- app/App.js
+++ app/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {render, findDOMNode} from 'react-dom';
 import solarSystem from '../data/solar_system.js';
 import scrollTo from 'scroll-into-view';
-
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 // describtion on move on move out
 // satelites?
@@ -295,11 +295,20 @@ class PlanetsList extends Component {
       return ( <a key={i}  id={planet.name} onClick={this.handleClick.bind(this, planet)}> {planet.name} </a>)
     })
     return (
-      <div className='navbar'>
-          <div className="wrap">
-            {planets}
+       <ReactCSSTransitionGroup 
+                                  transitionName="thing" 
+                                  transitionAppear={true} 
+                                  transitionAppearTimeout={400} 
+                                  transitionLeave={true} 
+                                  transitionLeaveTimeout={300}
+                                  transitionEnter={false} >
+          <div className='navbar'>
+              <div className="wrap">
+                {planets}
+              </div>
           </div>
-      </div>
+
+      </ReactCSSTransitionGroup>
     )
   }
 
