a9eb6d595848f7407f995f348a55c324408e20e6
Darya Murauyova <muravkina@yahoo.com>
2016-11-18 15:22:03 -0500

    apply styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
 /*
  * Default webpack configuration for development
  */
@@ -10,13 +10,6 @@ var config = {
     path: __dirname + "/dist",
     filename: "bundle.js"
   },
-    plugins: [
-  
-    
-        new ExtractTextPlugin('style.css', {
-          allChunks: false
-        })
-    ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
@@ -27,19 +20,11 @@ var config = {
       }
     },{
       test: /\.css$/,
-      loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader')
-    },
-      // Image URL config. Generate data URI's for images smaller than 10,000 bytes
-      {test: /\.(png|gif|jpe?g|svg)$/i, loader: 'url'},
-
-      // Image file config. Generate hashed file names to make them easy to cache.
-      {
+      loader: "style-loader!css-loader"
+    },       {
         test: /\.(png|gif|jpe?g|svg)$/i,
         loader: 'file?hash=sha512&digest=hex&name=[path][name]-[hash].[ext]'
       },
-
-      // Inline font files smaller than 10000 bytes
-      { test: /\.(woff2?|otf|eot|svg)$/, loader: 'url?limit=10000' },
 
       // File loader for fonts larger than 10000 bytes.
       { test: /\.(woff2?|otf|eot|svg)$/, loader: 'file?name=[path][name]-[name].[ext]' }]
@@ -55,18 +40,6 @@ var config = {
   }
 }
 
-/*
- * If bundling for production, optimize output
- */
-if (process.env.NODE_ENV === 'production') {
-  config.devtool = false;
-  config.plugins = [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({comments: false}),
-    new webpack.DefinePlugin({
-      'process.env': {NODE_ENV: JSON.stringify('production')}
-    })
-  ];
-};
+
 
 module.exports = config;
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -8,7 +8,8 @@ module.exports = {
     output: {
         path: __dirname + '/dist',
         filename: 'bundle.js',
-        publicPath:__dirname + '../public'
+        publicPath:'/solarsystem/dist',
+
     },
     plugins: [
         new webpack.optimize.OccurenceOrderPlugin(),
@@ -39,17 +40,11 @@ module.exports = {
       test: /\.css$/,
       loader: ExtractTextPlugin.extract('css-loader!postcss-loader')
     },
-      // Image URL config. Generate data URI's for images smaller than 10,000 bytes
-      {test: /\.(png|gif|jpe?g|svg)$/i, loader: 'url'},
-
       // Image file config. Generate hashed file names to make them easy to cache.
       {
         test: /\.(png|gif|jpe?g|svg)$/i,
         loader: 'file?hash=sha512&digest=hex&name=[path][name]-[hash].[ext]'
       },
-
-      // Inline font files smaller than 10000 bytes
-      { test: /\.(woff2?|otf|eot|svg)$/, loader: 'url?limit=10000' },
 
       // File loader for fonts larger than 10000 bytes.
       { test: /\.(woff2?|otf|eot|svg)$/, loader: 'file?name=[path][name]-[name].[ext]' }]
